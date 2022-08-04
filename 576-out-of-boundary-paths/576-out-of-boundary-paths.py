b952215c38ee885d0d2c86be1e8403ac8d7453e0
Nikhil Maurya <61533126+nik-ma@users.noreply.github.com>
2022-08-04 12:43:08 +0530

    Time: 216 ms (37.09%), Space: 19.3 MB (44.49%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -2,23 +2,23 @@ class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         # ans=0
         m,n=n,m
-
-        @cache
+        dp={}
         def solve(i,j,k):
             # nonlocal ans
             if k<0:
                 return 0
-            
             if (i<0 or i>n-1 or j<0 or j>m-1):
                 return 1
-
+            if (i,j,k) in dp:
+                return dp[(i,j,k)]
             ans=0
             ans+=solve(i+1,j,k-1)
             ans+=solve(i-1,j,k-1)
             ans+=solve(i,j+1,k-1)
             ans+=solve(i,j-1,k-1)
             # ans+=pt
-            return ans%(10**9+7)
+            dp[(i,j,k)]= ans%(10**9+7)
+            return dp[(i,j,k)]
         return solve(startRow,startColumn,maxMove)
         
         
