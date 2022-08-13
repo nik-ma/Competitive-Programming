b88492027ec3603076f13a71f5211a35a3f4973e
Nikhil Maurya <61533126+nik-ma@users.noreply.github.com>
2022-08-13 11:27:41 +0530

    Time: 71 ms (64.80%), Space: 14 MB (6.85%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,5 +1,5 @@
 class Solution:
-    def intToRoman1(self, num):
+    def intToRoman(self, num):
         values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
         numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
         res, i = "", 0
@@ -7,14 +7,5 @@ class Solution:
             res += (num//values[i]) * numerals[i]
             num %= values[i]
             i += 1
-        return res
-    
-    def intToRoman(self, num):
-        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
-        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
-        res = ""
-        for i, v in enumerate(values):
-            res += (num//v) * numerals[i]
-            num %= v
         return res
         
