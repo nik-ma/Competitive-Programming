53700a8f50aefea4ce17a80f9e7537d4896b38b5
Nikhil Maurya <61533126+nik-ma@users.noreply.github.com>
2022-08-16 15:52:10 +0530

    Time: 2082 ms (27.97%), Space: 29.9 MB (18.17%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canSeePersonsCount(self, A):
+        res = [0] * len(A)
+        stack = []
+        for i, v in enumerate(A):
+            while stack and A[stack[-1]] <= v:
+                res[stack.pop()] += 1
+            if stack:
+                res[stack[-1]] += 1
+            stack.append(i)
+        return res
